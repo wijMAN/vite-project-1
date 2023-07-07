aad391b2e534bd71ca3944b4306c484405a285ae
wijman <wish6242@gmail.com>
2023-07-08 03:06:17 +0530

    trial

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           <h1>TechyStar</h1>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
-            impedit fuga totam magni sunt deleniti facilis.
+            impedit fuga totam magni sunt deleniti facilis.Wij
           </p>
         </main>
       </div>
